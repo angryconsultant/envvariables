4e6576668e5c1c18215f9a301684ba6f9756c4ee
Daniel Björk <daniel.bjork@cloudium.se>
2024-04-18 12:17:12 +0200

    init

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myVariable = process.env.REACT_APP_MY_VARIABLE;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Environment Variable: {myVariable}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
